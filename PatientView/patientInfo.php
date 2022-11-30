a1b35a2215d2bb7867c51f28b0178f3bd85bb781
Gil Pena <69359634+gilpenajr@users.noreply.github.com>
2022-11-30 12:20:28 -0800

    adding php to pregnancies

--- PatientView/patientInfo.php
+++ PatientView/patientInfo.php
@@ -4,7 +4,6 @@
     // create query
     $sql =  "SELECT * FROM personal_info where id=1;";
     $result = $conn->query($sql); 
-
     //get row of patient information
     $row = $result->fetch_assoc();
     //getting patient name  
@@ -16,8 +15,26 @@
     //getting phone number 
     $patientPhone = $row["phone_number"];
 
-    
-    
-    //$users = mysqli_fetch_all($result, MYSQLI_ASSOC);
+    //// create query for pregnancies 
+    $pregnanciesSQL = "SELECT * FROM pregnancies where patient_ID = 2;";
+    $pregnanciesResult= $conn->query($pregnanciesSQL); 
 
+    
+    //function to print pregnancies 
+    function printPregnancies($pregnanciesResult){
+        $bool = true; 
+        while($bool){
+            $pregnancyRow = $pregnanciesResult->fetch_assoc(); 
+            if($pregnancyRow){
+                print_r($pregnancyRow); 
+                print("\n"); 
+            }
+            else{
+                $bool = false; 
+            }
+                
+            
+            
+        }  
+    }
 ?>
