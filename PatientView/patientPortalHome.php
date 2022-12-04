6b33e960616779c987d2a516478e94c80eef9762
Gil Pena <69359634+gilpenajr@users.noreply.github.com>
2022-12-04 12:29:57 -0800

    settings page and adding comments

--- PatientView/patientPortalHome.php
+++ PatientView/patientPortalHome.php
@@ -44,7 +44,7 @@
                         </a>
                         <div class="dropdown-menu" aria-labelledby="navbarDropdown">
 
-                            <a class="dropdown-item" href="patientSettings.php">Settings</a>
+                            <a class="dropdown-item" href="settings.php">Settings</a>
 
                             <a class="dropdown-item" href="../signout.php">Sign Out</a>
                         </div>
