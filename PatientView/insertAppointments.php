bc0e9f13165d52f4e3a130c7d26e1a598f46ab91
Gil Pena <69359634+gilpenajr@users.noreply.github.com>
2022-12-04 13:32:47 -0800

    Update insertAppointments.php

--- PatientView/insertAppointments.php
+++ PatientView/insertAppointments.php
@@ -8,10 +8,14 @@
     $startTime = new DateTime($_POST['startTime']); 
     $endTime = new DateTime($_POST['endTime']); 
     $difference = $endTime->diff($startTime); 
+    //length of appointment 
     $apptLength = $difference->format('%H:%I');
+    //appointment aproval to false/pending 
+    $apptApproved = 0; 
+
 
     #Query to appointments with new appointment
-    $insertAppointmentQuery = "INSERT INTO appointments(user_ID, start_date_time, end_date_time, appt_length) VALUES ('$patientID', '$apptStartDateTime', '$apptEndDateTime', '$apptLength'); ";
+    $insertAppointmentQuery = "INSERT INTO appointments(user_ID, start_date_time, end_date_time, appt_length, approved) VALUES ('$patientID', '$apptStartDateTime', '$apptEndDateTime', '$apptLength', 0); ";
     #print($insertAppointmentQuery); 
     if($conn->query($insertAppointmentQuery) == TRUE){
         header("Location: Appointments.php");
