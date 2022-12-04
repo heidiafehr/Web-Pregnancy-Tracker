717159741303386cb567f6a4de5b4ce406d89a5a
Gil Pena <69359634+gilpenajr@users.noreply.github.com>
2022-12-04 12:07:25 -0800

    update medications

--- PatientView/medications.php
+++ PatientView/medications.php
@@ -77,29 +77,22 @@
             $medOut = $medicationsResult->fetch_assoc(); 
             //if medications are found 
             if($medOut){
-                echo $medOut['med_name']; 
-
+                $medNameOut = $medOut['med_name'];
+                $medStartOut = $medOut['med_start_date'];
+                $medEndOut = $medOut['med_end_date'];
+                $medDesc = $medOut['med_description'];
+            }
+            else{
+                $medNameOut = 'No Prescriptions'; 
 
             }
-                
         ?>
         <tbody>
             <tr>
-                <th scope="row">1</th>
-                <td>Mark</td>
-                <td>Otto</td>
-                <td>@mdo</td>
-            </tr>
-            <tr>
-                <th scope="row">2</th>
-                <td>Jacob</td>
-                <td>Thornton</td>
-                <td>@fat</td>
-            </tr>
-            <tr>
-                <th scope="row">3</th>
-                <td colspan="2">Larry the Bird</td>
-                <td>@twitter</td>
+                <td><?php echo $medNameOut; ?></td>
+                <td><?php echo $medStartOut; ?></td>
+                <td><?php echo $medEndOut; ?></td>
+                <td><?php echo $medNameOut; ?></td>
             </tr>
         </tbody>
         </table>
