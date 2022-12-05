4f010651e2cd815a335f8b15a5e4b597758c1198
Heidi Fehr <heidi.a.fehr@gmail.com>
2022-12-05 05:35:54 -0800

    fixing some front-end on pregnancies.php

--- PatientView/pregnancies.php
+++ PatientView/pregnancies.php
@@ -1,23 +1,26 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Pregnancies</title>
+    <link rel="icon" type="image/x-icon" href="../images/baby-newborn.ico">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
     <link rel="stylesheet" href="patientStyle.css">
     <link rel="stylesheet" href="../style.css">
 </head>
+
 <body>
     <?php
-        include 'patientInfo.php';
-    ?> 
+    include 'patientInfo.php';
+    ?>
     <!-- NavBar Start -->
     <nav class="navbar navbar-expand-lg px-4" style="background-color: #6096ba; font-weight: 600;">
         <div class="container-fluid">
             <a class="navbar-brand" href="patientPortalHome.php">
-                <img src="../images/baby-newborn.png" alt="Logo" style="height:36px"/>
+                <img src="../images/baby-newborn.png" alt="Logo" style="height:36px" />
             </a>
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
@@ -40,7 +43,7 @@
                         <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
                             <?php
                             echo $row["first_name"];
-                                //include "get_first_name.php";
+                            //include "get_first_name.php";
                             ?>
                         </a>
                         <div class="dropdown-menu" aria-labelledby="navbarDropdown">
@@ -57,94 +60,91 @@
     <!-- NavBar End -->
 
     <!-- Pregnancy Card        -->
-    <div class="container">
-        <div class=" col">
-            <div class = "card" id="pregnancyCard">
-                <div class = "card-header"> 
-                    <h5>Pregnancies</h5>
-                </div> 
-                <div class = "card-body">
+    <div class="container mt-5">
+        <div class="col">
+            <div class="card" id="pregnancyCard">
+                <div class="card-header d-flex justify-content-between py-3">
+                    <h3>Pregnancies</h3>
+                    <!-- Button to show past pregnancies -->
+                    <tr>
+                        <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModal">
+                            Show Past Pregnancies
+                        </button>
+                    </tr>
+                </div>
+                <div class="card-body">
                     <!--Single Pregnancy Entry -->
                     <table class="table table-bordered table-hover">
                         <thead>
                             <tr>
-                            <th scope="col">Current Trimester </th>
-                            <th scope="col">Weeks Until Due Date</th>
-                            <th scope="col">Due Date</th>
-                            <th scope="col">Baby's Sex</th>
+                                <th scope="col">Current Trimester </th>
+                                <th scope="col">Weeks Until Due Date</th>
+                                <th scope="col">Due Date</th>
+                                <th scope="col">Baby's Sex</th>
                             </tr>
                         </thead>
-                    <tbody>
-                        <?php
+                        <tbody>
+                            <?php
                             //decarling vars
                             $firstPregancyRow = null;
-                            $countdownRatio = 0; 
+                            $countdownRatio = 0;
                             //getting current date and time 
-                            $tempDateTime = date('d-m-Y h:i:s a', time());    
+                            $tempDateTime = date('d-m-Y h:i:s a', time());
                             //saving date time as datetimeImmutabel
-                            $currentDateTime  = new dateTimeImmutable($tempDateTime); 
+                            $currentDateTime  = new dateTimeImmutable($tempDateTime);
                             //datetime string 
-                            $currDate = $currentDateTime->format('F d,Y'); 
-                            
+                            $currDate = $currentDateTime->format('F d,Y');
+
                             // create query for pregnancies 
                             $pregnanciesSQL = "SELECT * FROM pregnancies where patient_ID = $patientID;";
                             //get pregnacy rows if found 
-                            $pregnanciesResult= $conn->query($pregnanciesSQL);
-                            
+                            $pregnanciesResult = $conn->query($pregnanciesSQL);
+
                             //function to print current pregnancy
-                            if($pregnancyRow = $pregnanciesResult->fetch_assoc()){
+                            if ($pregnancyRow = $pregnanciesResult->fetch_assoc()) {
                                 //get due date 
                                 $dueDate = $pregnancyRow["due_date"];
                                 //converting due date to immutable 
                                 $dueDateImmutable = new dateTimeImmutable($dueDate);
                                 //getting difference for due date 
-                                $dueDateCountDown = $currentDateTime->diff($dueDateImmutable); 
+                                $dueDateCountDown = $currentDateTime->diff($dueDateImmutable);
                                 //getting days
-                                $countdownDays = $dueDateCountDown->format('%R%a'); 
-                                if($countdownDays <= 0){
-                                    echo "<td>No current pregnancies </td>"; 
-                                    $firstPregancyRow = $pregnancyRow; 
-                                }
-                                else{
+                                $countdownDays = $dueDateCountDown->format('%R%a');
+                                if ($countdownDays <= 0) {
+                                    echo "<td>No current pregnancies </td>";
+                                    $firstPregancyRow = $pregnancyRow;
+                                } else {
                                     //converting days left to weeks 
-                                    $countdownWeeks = floor($countdownDays/7); 
+                                    $countdownWeeks = floor($countdownDays / 7);
                                     //getting weeks into pregancy(40 weeks in a pregancy? according to bing)
-                                    $currentweek = 40-$countdownWeeks; 
+                                    $currentweek = 40 - $countdownWeeks;
                                     //getting countdown ratio for progress par 
-                                    $countdownRatio = ($countdownWeeks/40) * 100;
-                                    if($countdownWeeks <=13)
-                                        $trimester = "First"; 
-                                    elseif($countdownWeeks <=26)
+                                    $countdownRatio = ($countdownWeeks / 40) * 100;
+                                    if ($countdownWeeks <= 13)
+                                        $trimester = "First";
+                                    elseif ($countdownWeeks <= 26)
                                         $trimester = "2";
-                                    elseif($countdownWeeks <=40)
+                                    elseif ($countdownWeeks <= 40)
                                         $trimester = "Second";
-                                    echo(
-                                        "<td>$trimester</td>" . 
-                                        "<td>$countdownWeeks</td>" . 
-                                        "<td>" .$pregnancyRow["due_date"] .
-                                        "<td>". $pregnancyRow["baby_sex"] .
+                                    echo ("<td>$trimester</td>" .
+                                        "<td>$countdownWeeks</td>" .
+                                        "<td>" . $pregnancyRow["due_date"] .
+                                        "<td>" . $pregnancyRow["baby_sex"] .
                                         "</td></tr></div>"
-                                    ); 
+                                    );
                                 }
-                               
                             }
-                        ?>   
-                    </tbody>                     
+                            ?>
+                        </tbody>
                     </table>
                     <!-- Progress Bar -->
-                    <?php 
-                       echo"<div class='progress'>
+                    <?php
+                    echo "<div class='progress'>
                             <div class='progress-bar progress-bar-striped' role='progressbar'
                              aria-label='Default striped example' style='width: $countdownRatio%' 
                              aria-valuenow='100' aria-valuemin='0' aria-valuemax='100'></div>
-                        </div>"; 
-                        ?>
-                    <br>
-                    <!-- Button to show past pregnancies -->
-                    <tr>
-                    <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModal">
-                        Show Past Pregnancies
-                    </button></tr>
+                        </div>";
+                    ?>
                     <!-- Further pregnancies create more entries function or something -->
                 </div>
             </div>
@@ -163,35 +163,36 @@
                     <table class='table table-bordered table-hover'>
                         <thead>
                             <tr>
-                            <th scope="col">Weeks Until Due Date</th>
-                            <th scope="col">Due Date</th>
-                            <th scope="col">Baby's Sex</th>
+                                <th scope="col">Weeks Until Due Date</th>
+                                <th scope="col">Due Date</th>
+                                <th scope="col">Baby's Sex</th>
                             </tr>
                         </thead>
-                    <div class="card-body">
-                    <?php 
-                    //check for first pregnancy row 
-                        try{
-                            if($firstPregancyRow)
-                                printRow($firstPregancyRow); 
-                        }
-                        catch(Exception $e){
-                            print("nevermind"); 
-                        }
-                            
-                        //print following pregancies 
-                        while($pregnancyRow = $pregnanciesResult->fetch_assoc())
-                            printRow($pregnancyRow); 
-            
-                        function printRow($pregnancyRow){
-                            echo("
+                        <div class="card-body">
+                            <?php
+                            //check for first pregnancy row 
+                            try {
+                                if ($firstPregancyRow)
+                                    printRow($firstPregancyRow);
+                            } catch (Exception $e) {
+                                print("nevermind");
+                            }
+
+                            //print following pregancies 
+                            while ($pregnancyRow = $pregnanciesResult->fetch_assoc())
+                                printRow($pregnancyRow);
+
+                            function printRow($pregnancyRow)
+                            {
+                                echo ("
                                 <tr><td> Birthed </td>     
-                                <td>".$pregnancyRow["due_date"]."</td>" .
-                                "<td>".$pregnancyRow["baby_sex"]."</td>".
-                                "</tr></div>"
-                            );  
-                        }
-                    ?></table>
+                                <td>" . $pregnancyRow["due_date"] . "</td>" .
+                                    "<td>" . $pregnancyRow["baby_sex"] . "</td>" .
+                                    "</tr></div>"
+                                );
+                            }
+                            ?>
+                    </table>
                 </div>
                 <div class="modal-footer">
                     <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
@@ -201,6 +202,7 @@
     </div>
 
 
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-OERcA2EqjJCMA+/3y+gxIOqMEjwtxJY7qPCqsdltbNJuaOe923+mo//f6V8Qbsw3" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-OERcA2EqjJCMA+/3y+gxIOqMEjwtxJY7qPCqsdltbNJuaOe923+mo//f6V8Qbsw3" crossorigin="anonymous"></script>
 </body>
+
 </html>
